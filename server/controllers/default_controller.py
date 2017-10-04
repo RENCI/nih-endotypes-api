10cf7b6afcb1f286a423f8b07e67e02af914e481
hyi <hongyi@renci.org>
2017-10-04 15:33:11 -0400

    added sample mockup response

--- server/controllers/default_controller.py
+++ server/controllers/default_controller.py
@@ -1,4 +1,5 @@
 import connexion
+import json
 from models.body import Body
 from models.inline_response200 import InlineResponse200
 from datetime import date, datetime
@@ -10,12 +11,18 @@ from util import deserialize_date, deserialize_datetime
 def endotypes_get(body=None):
     """
     list of endotypes
-    list of endotypes
-    :param body: 
+    :param body:
     :type body: dict | bytes
 
     :rtype: List[InlineResponse200]
     """
     if connexion.request.is_json:
+        # for some reason connexion.request.get_json() returns null, need to figure out later
         body = Body.from_dict(connexion.request.get_json())
-    return 'do some magic!'
+        #if connexion.request.get_json():
+        #    return json.dumps(connexion.request.get_json())
+        #else:
+        #    return "Null"
+        with open('results.json', 'r') as f:
+            data = json.load(f)
+            return json.dumps(data)
