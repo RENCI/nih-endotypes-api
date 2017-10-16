2cb0f645f0542b480c71ef04664034e488e132c2
hyi <hongyi@renci.org>
2017-10-16 10:13:14 -0400

    changed server models accordingly due to swagger api spec changes

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -6,4 +6,5 @@ from models.input import Input
 from models.endotypes_period import EndotypesPeriod
 from models.exposure import Exposure
 from models.response200 import Response200
+from models.response200_output import Response200Output
 from models.visit import Visit
